b7524f142c43f65e239c33be3e127d44f82e2d59
DanielLaborda <dani.laborda90@gmail.com>
2021-04-02 16:25:14 +0200

    created recent posts and search bar components and used them in the home component

---
+++ src/components/recentPost.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class RecentPost extends Component {
+  render() {
+    return (
+        <div class="recent-posts">
+            <div className="recent-post-wrapper">
+                <div className="recentposts-heading">Recent Posts</div>
+                <ul className="recent-post-posts">
+                    <li>Recent post 0</li>
+                    <li>Recent post 1</li>
+                    <li>Recent post 2</li>
+                </ul>
+            </div>
+        </div>
+    )
+  }
+}
